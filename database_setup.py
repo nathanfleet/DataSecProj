9344af4cdacb12d0effe7c4cb936a0c50a9a6253
Phoebe <122624622+pmasters26@users.noreply.github.com>
2024-11-22 14:50:02 -0500

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,32 @@
+import sqlite3
 
+connection = sqlite3.connect('Masters.db')
+cursor = connection.cursor()
+
+# Creating the 'users' table
+sql_command = """CREATE TABLE users (
+    doctor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    encrypted_password TEXT,
+    iv TEXT,
+    hashed_password TEXT,
+    secret_key TEXT 
+);"""
+cursor.execute(sql_command)
+
+# Creating the 'medical_table' table
+sql_command = """CREATE TABLE medical_table (
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender VARCHAR(1),
+    age INT, 
+    weight FLOAT,
+    height FLOAT, 
+    doctor_id INTEGER,
+    FOREIGN KEY (doctor_id) REFERENCES users(doctor_id)
+);"""
+cursor.execute(sql_command)
+
+# Commit the changes and close the connection
+connection.commit()
+connection.close()
